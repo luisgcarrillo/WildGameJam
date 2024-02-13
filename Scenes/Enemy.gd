c62859c9300876b9d43c5902cf0040bbbfaa6c02
bennett <14241775+bennettdaley@users.noreply.github.com>
2024-02-12 22:33:38 -0600

    Created enemy
    Enemy follows you. Hope this doesn't break anything!!

---
+++ Scenes/Enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@onready var player = get_parent().get_node("Player")
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var ACCEL = 10000.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	var player_position = player.global_position
+	var target_position = (player_position - position).normalized()
+	velocity = velocity.move_toward(target_position * SPEED, ACCEL * delta)
+	
+	move_and_slide()
+	look_at(player_position)
