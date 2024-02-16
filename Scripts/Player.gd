60e90a5187a4c44e5a7a4c7f0b4394d2faa70b28
AJnStuff <adolfoajcarrillo@gmail.com>
2024-02-15 22:36:34 -0600

    Projectiles
    Basic projectiles added. Left click to shoot.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -19,5 +19,16 @@ func _physics_process(delta):
 		velocity.x -= SPEED
 	if Input.is_action_pressed("Right"):
 		velocity.x += SPEED
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		shoot_projectile( get_local_mouse_position().x, get_local_mouse_position().y)
 
 	move_and_slide()
+
+func shoot_projectile(x,y):
+	const BULLET = preload("res://Scenes/projectile.tscn")
+	var bullet = BULLET.instantiate()
+	get_parent().add_child(bullet)
+	bullet.position = Vector2(position.x,position.y)
+	#bullet.angular_velocity = 2000
+	bullet.add_constant_central_force(Vector2(x,y))
+	
